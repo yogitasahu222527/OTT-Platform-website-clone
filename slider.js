6aba69e67624af0ff1121208ae27038d2f4c6848
yogitasahu1225 <155805946+yogitasahu222527@users.noreply.github.com>
2024-05-02 07:03:26 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,22 @@
+let movies = [
+ 
+    {
+      name: "Avengers : Endgame ",
+      des: " Time travel, sacrifice, and an ultimate battle shape the thrilling conclusion to this expansive cinematic saga.",
+      image: "images/slider 3.JPG",
+    },
+    
+
+    {
+      name: "Hanuman",
+      des: " Hanuman is a great superhero film that is sure to please audiences of all ages .  The legendary Hindu deity Hanuman embarks on new adventures, showcasing his unwavering devotion, valor, and divine prowess through epic battles that leave a lasting impact on mythology.",
+      image: "images/slider 1.JPEG",
+    },
+    {
+      name: "luca",
+      des: " Luca is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. ",
+      image: "images/slider 2.PNG",
+    },
+   
+
+  ];
